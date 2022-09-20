a2914f035ca65daa53ceedeb8a086c75e9d11cfa
Paul Saunders <paul.saunders@finbourne.com>
2022-09-20 15:10:38 +0100

    CTECH-1566: Updates integration tests.
    Signed-off-by: Paul Saunders <paul.saunders@finbourne.com>

--- sdk/Finbourne.Insights.Sdk.Extensions.IntegrationTests/TokenProviderConfigurationTest.cs
+++ sdk/Finbourne.Insights.Sdk.Extensions.IntegrationTests/TokenProviderConfigurationTest.cs
@@ -5,11 +5,24 @@ namespace Finbourne.Insights.Sdk.Extensions.IntegrationTests
     [TestFixture]
     public class TokenProviderConfigurationTest
     {
+        private static readonly Lazy<ApiConfiguration> ApiConfig =
+            new Lazy<ApiConfiguration>(() => ApiConfigurationBuilder.Build("secrets.json"));
+
         //Test requires [assembly: InternalsVisibleTo("namespace Finbourne.Insights.Sdk.Extensions.IntegrationTests")] in ClientCredentialsFlowTokenProvider
         [Test]
         public void Construct_AccessToken_NonNull()
         {
-            var config = new TokenProviderConfiguration(new ClientCredentialsFlowTokenProvider(ApiConfigurationBuilder.Build("secrets.json")));
+            ITokenProvider tokenProvider;
+            if (ApiConfig.Value.MissingSecretVariables)
+            {
+                tokenProvider = new PersonalAccessTokenProvider(ApiConfig.Value.PersonalAccessToken);
+            }
+            else 
+            {
+                tokenProvider = new ClientCredentialsFlowTokenProvider(ApiConfigurationBuilder.Build("secrets.json")); 
+            }
+
+            var config = new TokenProviderConfiguration(tokenProvider);
             Assert.IsNotNull(config.AccessToken);
         }
     }
